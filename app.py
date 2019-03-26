17a4ac07cf1e0be5ee536dc35f00ae7f2f18b939
Yu-Chien Huang <yc_huang@mit.edu>
2019-03-26 12:34:14 -0400

    modified the code in app.py so that the plot is correctly  updated dynamically

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, flash
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField
 from day10 import *
 import os
+import base64
+import io
 
 
 class ReusableForm(Form):
@@ -29,15 +31,26 @@ def graph():
 
 @app.route('/about', methods=['GET','POST'])
 def about():
+  if os.path.exists('public/plot.png'):
+    os.remove('public/plot.png')
   form = ReusableForm(request.form)
   print(form.errors)
   if request.method == 'POST':
       name=request.form['name']
       month=request.form['month']
       #print(name, month, type(month))
+      
+      img = io.BytesIO()
       fig = df_one_month_closing_price(2017,int(month),name)
-      fig.savefig(os.path.join(app.root_path, 'public/plot'))
-      return render_template('graph.html',  url ='/public/plot.png')
+      #fig.savefig(os.path.join(app.root_path, 'public/plot'))
+      fig.savefig(img, format='png')
+      img.seek(0)
+      plot_url = base64.b64encode(img.getvalue()).decode()
+      
+      #return '<img src="data:image/png;base64,{}">'.format(plot_url)
+
+      
+      return render_template('graph.html',plot_url=plot_url)
       
       
       
